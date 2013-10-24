f4451d17638064a51f2c858caac722b08bda9a8c
joaoarthurbm <joaoarthurbm@gmail.com>
2013-10-24 15:08:02 -0700

    stats data

--- data/generete-freqs.sh
+++ data/generete-freqs.sh
@@ -1,31 +1,63 @@
-## issue comments##
+## issue comments ##
 
-# comments per issue
-cut -d' ' -f2 project-issue-comment-developer.data | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > comments-per-issue.freq
+# comments per issue commented
+cut -d' ' -f2 project-issue-comment-developer.data | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > com-comments-per-issue.freq
 
-# developers per issue
-cut -d' ' -f2,4 project-issue-comment-developer.data | sort | uniq -c | sort -rn | awk -F' ' '{pnt $2}' |  sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > developers-per-issue.freq
+# commenter per issue
+cut -d' ' -f2,4 project-issue-comment-developer.data | sort | uniq -c | sort -rn | awk -F' ' '{print $2}' |  sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > com-commenter-per-issue.freq
 
 # issue comment per developer
-cut -d' ' -f4 project-issue-comment-developer.data | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > issuecomments-per-developer.freq
+cut -d' ' -f4 project-issue-comment-developer.data | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > com-issuecomments-per-developer.freq
 
 # issue per developer
-cut -d' ' -f2,4 project-issue-comment-developer.data | sort | uniq -c | awk -F' ' '{print $NF}' | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > issue-per-developer.freq
+cut -d' ' -f2,4 project-issue-comment-developer.data | sort | uniq -c | awk -F' ' '{print $NF}' | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > com-issue-per-developer.freq
 
 # issue per project
-cut -d' ' -f1,2 project-issue-comment-developer.data | sort | uniq -c | awk -F' ' '{print $2}' | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > issues-per-proj.freq
+cut -d' ' -f1,2 project-issue-comment-developer.data | sort | uniq -c | awk -F' ' '{print $2}' | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > com-issue-per-proj.freq
 
 # issue comment per project
-cut -d' ' -f1 project-issue-comment-developer.data | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' >  issuecomments-per-proj.freq
+cut -d' ' -f1 project-issue-comment-developer.data | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' >  com-issuecomments-per-proj.freq
 
 ## commit comments ##
 
 # commit comments per project
-cut -d' ' -f1 project-commit-comment-developer.data | sort | uniq -c | sort -rn | sed 's/^[ ]*//g'> commit-comments-per-project.freq
+cut -d' ' -f1 project-commit-comment-developer.data | sort | uniq -c | sort -rn | sed 's/^[ ]*//g'> com-commit-comments-per-project.freq
 
 # comments per commit
-cut -d' ' -f2 project-commit-comment-developer.data | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > comments-per-commit.freq
+cut -d' ' -f2 project-commit-comment-developer.data | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > com-comments-per-commit.freq
+
+# comments per developer
+cut -d' ' -f4 project-commit-comment-developer.data | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > com-commit-comments-per-developer.freq
 
 # commenter per commit
-cut -d' ' -f2,4 project-commit-comment-developer.data | sort | uniq -c | awk -F' ' '{print $(NF-1)}' | sort | uniq -c | sort -rn | sed 's/^[ [ ]*//g' > commenter-per-commit.freq
+cut -d' ' -f2,4 project-commit-comment-developer.data | sort | uniq -c | awk -F' ' '{print $(NF-1)}' | sort | uniq -c | sort -rn | sed 's/^[ [ ]*//g' > com-commenter-per-commit.freq
 
+# commits per project
+cut -d' ' -f1,2 project-commit-comment-developer.data | sort | uniq -c | awk -F' ' '{print $2}' | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > com-commit-per-proj.freq
+
+# commits per developer
+cut -d' ' -f2,4 project-commit-comment-developer.data | sort | uniq -c | awk -F' ' '{print $NF}' | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > com-commit-per-developer.freq
+
+## Pull request comments ##
+
+# commenter per pull
+cut -d' ' -f2,5 project-pull-comment-commit-developer.data | sort | uniq -c | awk -F' ' '{print $(NF-1)}' | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > com-commenter-per-pull.freq
+
+# comments per pull
+cut -d' ' -f2 project-pull-comment-commit-developer.data | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > com-comments-per-pull.freq
+
+# pull comments per project
+cut -d' ' -f1 project-pull-comment-commit-developer.data | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > com-pull-comments-per-project.freq
+
+# pull comments per developer
+cut -d' ' -f5 project-pull-comment-commit-developer.data | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > com-pull-comments-per-developer.freq
+
+# pull per developer
+cut -d' ' -f2,5 project-pull-comment-commit-developer.data | sort | uniq -c | awk -F' ' '{print $NF}' | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > com-pull-per-developer.freq
+
+# pull per project
+cut -d' ' -f1,2 project-pull-comment-commit-developer.data | sort | uniq -c | awk -F' ' '{print $2}' | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > com-pull-per-project.freq
+
+## General stats
+# developer per project
+cut -d' ' -f1 project-collaborator.data | sort | uniq -c | sort -rn | sed 's/^[ ]*//g' > all-developers-per-project.freq
