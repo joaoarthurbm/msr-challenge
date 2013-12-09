921e8627fe629380e3815d5164e0210145084e05
joaoarthurbm <joaoarthurbm@gmail.com>
2013-12-09 13:17:59 -0800

    sentences update; classifier update

--- data/discussions/design/classify.py
+++ data/discussions/design/classify.py
@@ -6,5 +6,5 @@ cl = NaiveBayesClassifier("sentences.csv", format="csv")
 for line in open('comments.data','r'):
 	blob = TextBlob(line)
 	for sentence in blob.sentences:
-		print sentence, cl.classify(sentence)
+		print sentence,cl.classify(sentence)
 
